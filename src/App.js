429264233ccf69ac47421967a93fb9956ccd32ac
annadpp <71819431+annadpp@users.noreply.github.com>
2020-11-28 12:08:11 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.linkedin.com/in/annadepablopuig/" target="_blank">
-          Anna de Pablo Puig
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/annadpp/react_final_project"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/annadepablopuig/"
+            target="_blank"
+          >
+            Anna de Pablo Puig
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/annadpp/react_final_project"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
