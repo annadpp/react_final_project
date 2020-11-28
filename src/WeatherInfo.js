1b5260e997c2228c9a8f73d7297f610bcc729550
annadpp <71819431+annadpp@users.noreply.github.com>
2020-11-28 18:47:21 +0100

    Search engine + icon

---
+++ src/WeatherInfo.js
@@ -0,0 +1,38 @@
+import React from "react";
+import FormattedDate from "./FormatedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.desc}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src={props.data.iconUrl}
+              alt={props.data.description}
+              className="float-left"
+            />
+
+            <div className="float-left">
+              <span className="temp">{props.data.temp}</span>
+              <span className="unit">ºC</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humid} %</li>
+            <li>Wind: {props.data.wind} km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
