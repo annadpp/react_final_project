826afcd5c1ebb4eb9a4b4c7d65c908f6285bcb6d
annadpp <71819431+annadpp@users.noreply.github.com>
2020-11-28 11:47:49 +0100

    added URLs to personal info

--- src/App.js
+++ src/App.js
@@ -3,5 +3,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.linkedin.com/in/annadepablopuig/" target="_blank">
+          Anna de Pablo Puig
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/annadpp/react_final_project"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
